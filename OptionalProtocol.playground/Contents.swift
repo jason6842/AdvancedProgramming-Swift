68cf4353d675c12a2cc99a7efbc60d9a37e1b254
Jason Ma <maj14@uci.edu>
2024-11-27 00:18:51 -0800

    Optional Protocols require @objc protocol

--- OptionalProtocol.playground/Contents.swift
+++ OptionalProtocol.playground/Contents.swift
@@ -1,3 +1,19 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+@objc protocol ReseravationDataSource {
+    @objc optional func reserve(tables: Int) -> Int // Optional
+    @objc optional var maxReservableTables: Int { get }
+}
+
+class ReservationWebsite: ReseravationDataSource {
+    func reserve(tables: Int) -> Int {
+        if tables < 1 {
+            return 0
+        }
+        return tables
+    }
+}
+
+class ReservationAgent: ReseravationDataSource {
+    let maxReservableTables: Int = 2
+}
