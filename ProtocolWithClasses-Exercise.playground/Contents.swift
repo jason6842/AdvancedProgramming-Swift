84e62a999e178b7b8257df65572c70a847ddfd6b
Jason Ma <maj14@uci.edu>
2024-11-29 22:59:28 -0800

    Protocols is similar to Java Interfaces

--- ProtocolWithClasses-Exercise.playground/Contents.swift
+++ ProtocolWithClasses-Exercise.playground/Contents.swift
@@ -1,3 +1,28 @@
-import UIKit
+protocol Dish {
+    var name: String { get }
+    var preparationMinutes: Int { get set }
+    func prepare()
+    func plate(artisticLevel: Int)
+}
 
-var greeting = "Hello, playground"
+class MainDish: Dish {
+    let name: String
+    var preparationMinutes: Int
+    
+    init(name: String, preparationMinutes: Int) {
+        self.name = name
+        self.preparationMinutes = preparationMinutes
+    }
+    
+    func prepare() {
+        print("Preparing \(name) for \(preparationMinutes) minutes")
+    }
+    
+    func plate(artisticLevel: Int) {
+        print("Plating artistic level: \(artisticLevel)")
+    }
+}
+
+var mainDish = MainDish(name: "Super Spaghetti", preparationMinutes: 35)
+mainDish.prepare()
+mainDish.plate(artisticLevel: 10)
