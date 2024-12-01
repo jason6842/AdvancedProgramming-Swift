9b0ebe93a3aedc98d4dfd6b34cfa264c9b3bdb08
Jason Ma <maj14@uci.edu>
2024-12-01 14:41:29 -0800

    Map, Filter, and Reduce

--- MapFilterReduce.playground/Contents.swift
+++ MapFilterReduce.playground/Contents.swift
@@ -1,3 +1,30 @@
-import UIKit
+let numbersArray = [1, 2, 3, 4, 5]
 
-var greeting = "Hello, playground"
+// Map
+let numbersAsStrings = numbersArray.map { (aNumber) -> String in return String(aNumber) }
+print(numbersAsStrings)
+
+// Map - Shorthand argument names
+let numbersAsStrings1 = numbersArray.map { String($0) }
+print(numbersAsStrings1)
+
+// Filter
+let onlySmallNumbers = numbersArray.filter { (aNumber) -> Bool in return aNumber < 3 }
+print(onlySmallNumbers)
+
+// Filter - Shorthand argument names
+let onlySmallNumbers1 = numbersArray.filter { $0 < 3 }
+print(onlySmallNumbers1)
+
+// Reduce
+let sumOfAllNumbers = numbersArray.reduce(0) { (resultSoFar, currentNumber) -> Int in
+        return resultSoFar + currentNumber
+}
+print(sumOfAllNumbers)
+
+// Reduce - Shorthand argument names
+let sumOfAllNumbers1 = numbersArray.reduce(0) { $0 + $1 }
+print(sumOfAllNumbers1)
+
+let finalListString = numbersAsStrings.reduce("") { $0 +  "," + $1 }
+print(finalListString)
--- MapFilterReduceExamples.playground/Contents.swift
+++ MapFilterReduceExamples.playground/Contents.swift
@@ -1,3 +1,65 @@
-import UIKit
+struct User {
+    let id: Int
+    let name: String
+}
 
-var greeting = "Hello, playground"
+let users = [
+    User(id: 1, name: "John"),
+    User(id: 2, name: "Tom")
+]
+
+let usersId = users.map { user in
+    return user.id
+}
+print(usersId) // [1, 2]
+
+import Foundation
+
+// Compact map - is a special type of map that discards any nil values. It is very useful
+// if an object conversion function could produce nil. In this case, however, you only
+// want the non-nil values in the resulting array.
+let urlsAsStrings = ["https://www.google.com", "not a valid url"]
+let urlsOrNil = urlsAsStrings.map { URL(string: $0) }
+let urls = urlsAsStrings.compactMap { URL(string: $0) }
+print(urlsOrNil)
+print(urls)
+
+// Flat map - The mapping closure returns an array of elements rather than just a single element,
+// and The final resulting array must be a one-dimensional array. This means that there must not
+// be an array inside of an array
+
+struct User1 {
+    let id: Int
+    let name: String
+    let languages: [String]
+}
+
+let users1 = [
+    User1(id: 1, name: "John", languages: ["English, German"]),
+    User1(id: 2, name: "Tom", languages: ["Russian", "Spanish"])
+]
+let allLanguages = users1.flatMap { $0.languages }
+print(allLanguages)
+
+
+struct Order {
+    let price: Int
+    let location: String
+}
+
+func totalRevenueOf(orders: [Order], location: String) -> Int {
+    let ordersAtLocation = orders.filter { $0.location == "New York" }
+    print(ordersAtLocation)
+    let orderPrices = ordersAtLocation.map { $0.price }
+    print(orderPrices)
+    return orderPrices.reduce(0) { $0 + $1}
+}
+
+let orders = [
+    Order(price: 24, location: "New York"),
+    Order(price: 37, location: "San Francisco"),
+    Order(price: 101, location: "New York"),
+]
+
+let result = totalRevenueOf(orders: orders, location: "New York")
+print(result)
