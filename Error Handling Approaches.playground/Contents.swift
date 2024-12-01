01200e22f216bd30ab01ca105be94b3eef3ccb44
Jason Ma <maj14@uci.edu>
2024-12-01 13:32:44 -0800

    Error handling with different try approaches

--- Error Handling Approaches.playground/Contents.swift
+++ Error Handling Approaches.playground/Contents.swift
@@ -1,3 +1,64 @@
-import UIKit
+/*
+ try! and try? expresions
+ 
+ The try! expression lets you call a throwable function outside of the do-catch statement. It
+ disables the error handling and should be used in situations where you know that the particular
+ throwable function, will not, in fact, throw errors. If an error is thrown, the application
+ will crash. Using the try! expression in the production is not advisible.
+ 
+ The try? expression also lets you call the throwable function outside of the do-catch statement.
+ The expression lets you write concise error-handling code when you to handle all the errors in
+ the same way.
+ 
+ The try? expression makes the result of the function call an optional value. The result will
+ either be the result of the functin call, or, if an error was thrown - it will be converted
+ to a nil result value.
+ */
 
-var greeting = "Hello, playground"
+// When to use try!
+// 1. Loading a file from a disk that is guaranteed to be there and you know will never fail.
+// 2. Writing unit test to simplify code
+
+// When to use try?
+// 1. The error thrown is not important, all errors are handled this way.
+
+// Same approach to use try!
+/*
+do {
+    let image = try loadImageThrowableFunction("image.png")
+} catch{}
+let image = try! loadImageThrowableFunction("image.png")
+*/
+
+// Same approach to use try?
+/*
+ do {
+    let image = try loadImageThrowableFunction("image.png")
+ } catch {}
+ 
+ // Either the image or returns nil or not found
+ let image = try? loadImageThrowableFunction("image.png")
+ */
+
+class Calculator {
+    func divide (x: Double, y: Double) throws -> Double {
+        if y == 0 {
+            throw CalculatorError.divisionByZero
+        }
+        return x / y
+    }
+}
+
+enum CalculatorError: Error {
+    case divisionByZero
+}
+
+let calculator = Calculator()
+do {
+    let successfulResult = try calculator.divide(x: 10, y: 2)
+    print(successfulResult)
+    let invalidResult = try calculator.divide(x: 10, y: 0)
+    print(invalidResult)
+} catch CalculatorError.divisionByZero {
+    print("Division by zero detected and not allowed")
+}
--- Error Handling.playground/Contents.swift
+++ Error Handling.playground/Contents.swift
@@ -8,3 +8,33 @@ func validatePassword(password: String) throws {
         throw PasswordValidationError.passwordTooShort
     }
 }
+
+enum RegistrationDetailsFormatError: Error {
+    case usernameEmpty
+    case passwordEmpty
+    case passwordTooShort
+}
+
+func validateUserDetails(username: String, password: String) throws {
+    if username.isEmpty {
+        throw RegistrationDetailsFormatError.usernameEmpty
+    } else if password.isEmpty {
+        throw RegistrationDetailsFormatError.passwordEmpty
+    } else if password.count < 7 {
+        throw RegistrationDetailsFormatError.passwordTooShort
+    }
+}
+
+do {
+    try validateUserDetails(username: "JohnAppleseed", password: "example")
+    print("Validation sucess")
+} catch RegistrationDetailsFormatError.usernameEmpty,
+        RegistrationDetailsFormatError.passwordEmpty {
+    print("Username and password should not be empty")
+} catch RegistrationDetailsFormatError.passwordTooShort {
+    print("Password too short")
+} catch {
+    print(error)
+}
+
+
